ce77c2af75e0683f5c63c0cd499be6e2b29ca1ac
Joshua Thorp <joshua@stigmergic.net>
2013-01-29 10:30:41 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='social',
+    version='0.0.1',
+    description='Django app for easily including a social content api.',
+    author='MadeinHaus',
+    author_email='cms-admin@madeinhaus.com',
+    url='https://github.com/MadeInHaus/django-social',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
