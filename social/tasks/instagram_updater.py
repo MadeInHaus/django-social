c3c5bde8d7f8191d600f135c09ef797c2eca5517
Dino Petrone <dino@madeinhaus.com>
2013-02-05 18:16:48 -0800

    adding instagram updater

---
+++ social/tasks/instagram_updater.py
@@ -0,0 +1,25 @@
+import time
+from .. import settings
+
+#from ..models import FacebookAccount, FacebookMessage
+from celery.utils.log import get_task_logger
+from instagram.client import InstagramAPI
+
+log = get_task_logger(__name__)
+
+
+class InstagramUpdater():
+    def __init__(self):
+        self._access_token = None
+        self._app_id = settings.SOCIAL_INSTAGRAM_CLIENT_ID
+        self._app_secret = settings.SOCIAL_INSTAGRAM_CLIENT_SECRET
+        self.api = InstagramAPI(client_id=settings.SOCIAL_INSTAGRAM_CLIENT_ID,
+                                client_secret=settings.SOCIAL_INSTAGRAM_CLIENT_SECRET,
+                                redirect_uri='http://127.0.0.1:8000')
+
+
+
+    def update(self):
+        response = self.api.tag_search('#face')
+        print(response)
+        
