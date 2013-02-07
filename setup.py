ca2b84b2e10e9df0ee7083cb33bb1744a9f9d347
Dino Petrone <dino@madeinhaus.com>
2013-02-07 14:12:17 -0800

    fixing setup

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     url='https://github.com/MadeInHaus/django-social',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['git+ssh://git@github.com/MadeInHaus/twython.git','gevent==0.13.8'],
+    dependency_links=['git+ssh://git@github.com/MadeInHaus/twython.git'],
+    install_requires=['gevent==0.13.8'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
