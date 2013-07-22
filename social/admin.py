782203cb781c4a033991a03925914eb0372e96b3
Rafael Valverde <rafacvo@gmail.com>
2013-07-22 15:26:01 -0700

    Add singleton admin models for social network settings.

--- social/admin.py
+++ social/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-
-from django.template.response import TemplateResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 from .models import FacebookAccount, FacebookMessage, TwitterAccount, TwitterMessage,\
-                    TwitterSearch, RSSAccount, RSSMessage, Message, InstagramSearch, InstagramMessage
-from .settings import SOCIAL_TWITTER_CONSUMER_KEY, SOCIAL_TWITTER_CONSUMER_SECRET
+                    TwitterSearch, RSSAccount, RSSMessage, Message, InstagramSearch, InstagramMessage, \
+                    TwitterSetting, FacebookSetting, InstagramSetting, RSSSetting
 from .views import begin_auth
 
 from logging import getLogger
-from django.http import HttpResponseRedirect
-import traceback
-from django.core.urlresolvers import reverse
 
 log = getLogger(__name__)
 
@@ -32,6 +30,72 @@ def favorite_message(modeladmin, request, queryset):
         item.status = 5
         item.save()
 favorite_message.short_description = "Mark As Favorite"
+
+def admin_url(model, url, object_id=None):
+    """
+    Returns the URL for the given model and admin url name.
+    """
+    opts = model._meta
+    url = "admin:%s_%s_%s" % (opts.app_label, opts.object_name.lower(), url)
+    args = ()
+    if object_id is not None:
+        args = (object_id,)
+    return reverse(url, args=args)
+
+class SingletonAdmin(admin.ModelAdmin):
+    """
+    Admin class for models that should only contain a single instance
+    in the database. Redirect all views to the change view when the
+    instance exists, and to the add view when it doesn't.
+    """
+
+    def handle_save(self, request, response):
+        """
+        Handles redirect back to the dashboard when save is clicked
+        (eg not save and continue editing), by checking for a redirect
+        response, which only occurs if the form is valid.
+        """
+        form_valid = isinstance(response, HttpResponseRedirect)
+        if request.POST.get("_save") and form_valid:
+            return redirect("admin:index")
+        return response
+
+    def add_view(self, *args, **kwargs):
+        """
+        Redirect to the change view if the singleton instance exists.
+        """
+        try:
+            singleton = self.model.objects.get()
+        except (self.model.DoesNotExist, self.model.MultipleObjectsReturned):
+            kwargs.setdefault("extra_context", {})
+            kwargs["extra_context"]["singleton"] = True
+            response = super(SingletonAdmin, self).add_view(*args, **kwargs)
+            return self.handle_save(args[0], response)
+        return redirect(admin_url(self.model, "change", singleton.id))
+
+    def changelist_view(self, *args, **kwargs):
+        """
+        Redirect to the add view if no records exist or the change
+        view if the singleton instance exists.
+        """
+        try:
+            singleton = self.model.objects.get()
+        except self.model.MultipleObjectsReturned:
+            return super(SingletonAdmin, self).changelist_view(*args, **kwargs)
+        except self.model.DoesNotExist:
+            return redirect(admin_url(self.model, "add"))
+        return redirect(admin_url(self.model, "change", singleton.id))
+
+    def change_view(self, *args, **kwargs):
+        """
+        If only the singleton instance exists, pass ``True`` for
+        ``singleton`` into the template which will use CSS to hide
+        the "save and add another" button.
+        """
+        kwargs.setdefault("extra_context", {})
+        kwargs["extra_context"]["singleton"] = self.model.objects.count() == 1
+        response = super(SingletonAdmin, self).change_view(*args, **kwargs)
+        return self.handle_save(args[0], response)
 
 class MessageAdmin(admin.ModelAdmin):
     actions = [approve_message, rejected_message, favorite_message]
@@ -56,6 +120,11 @@ class MessageAdmin(MessageAdmin):
 class InstagramMessageAdmin(MessageAdmin):
     list_display = ('id','admin_image_low','message', 'status')
 
+
+admin.site.register(TwitterSetting, SingletonAdmin)
+admin.site.register(FacebookSetting, SingletonAdmin)
+admin.site.register(InstagramSetting, SingletonAdmin)
+admin.site.register(RSSSetting, SingletonAdmin)
 
 admin.site.register(FacebookAccount)
 admin.site.register(FacebookMessage, MessageAdmin)
