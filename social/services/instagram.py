06101f9eaf6bc4852b8dceb2a2dc9945d6ae0533
Rafael Valverde <rafacvo@gmail.com>
2013-08-12 02:09:24 -0700

    Revamp InstagramUpdater.

--- social/services/instagram.py
+++ social/services/instagram.py
@@ -1,4 +1,3 @@
-import gevent
 import requests
 
 
@@ -7,60 +6,41 @@ MEDIA_ACCEPT_PARAMETERS = ["count", "max_id"]
 class RateLimitException(Exception): pass
 
 class InstagramAPI(object):
-    def __init__(self, app_id=None):
-        self._app_id = app_id
+    def __init__(self, account):
+        self._account = account
 
-    def _scrap_accounts(self):
-        threads = []
-
-        for account in self.accounts:
-            if account.scrap_profile:
-                threads.append(gevent.spawn(self._scrap_account, account))
-
-        gevent.joinall(threads)
-
-    def _scrap_account(self, account, max_photos=100):
-        url = 'https://api.instagram.com/v1/users/3/media/recent/' \
-              '?access_token={}'.format(account.access_token)
-        total_pictures = 0
-
-        while url:
-            response = self._get_data_for_url(url)
-            content = response.json()
-            url = content.get('pagination', {}).get('next_url')
-
-            for photo in content['data']:
-                if total_pictures > max_photos:
-                    return
-                else:
-                    total_pictures += 1
-                    yield photo
-
-    def _get_data_for_url(self, url, params={}):
-        params['client_id'] = self._app_id
-        response = requests.get(url, params=params)
+    def _get_data_for_url(self, url):
+        response = requests.get(url, params={
+            'access_token': self._account.access_token,
+        })
 
         if not response.ok and response.json()['error_type'] == 'OAuthRateLimitException':
             raise RateLimitException()
         else:
             return response
 
-    def tag_recent_media(self, tag, count=100):
-        url = 'https://api.instagram.com/v1/tags/{}/media/recent'.format(tag)
-        total_sent = 0
+    def _retrieve_photos(self, url, max_photos):
+        total_pictures = 0
 
         while url:
-            res = self._get_data_for_url(url)
-            content = res.json()
-            if not res.ok:
+            response = self._get_data_for_url(url)
+            content = response.json()
+            if not response.ok:
                 raise RuntimeError(content['meta']['error_message'])
 
-            messages = content.get('data', [])
-
-            for message in messages:
-                yield message
-                total_sent += 1
-                if total_sent >= count:
-                    raise StopIteration()
-
             url = content.get('pagination', {}).get('next_url')
+
+            for photo in content.get('data', []):
+                if total_pictures >= max_photos:
+                    return
+                else:
+                    total_pictures += 1
+                    yield photo
+
+    def scrap_account(self, max_photos=100):
+        url = 'https://api.instagram.com/v1/users/{}/media/recent/'.format(self._account.instagram_id)
+        return self._retrieve_photos(url, max_photos)
+
+    def search_tag(self, tag, max_photos=100):
+        url = 'https://api.instagram.com/v1/tags/{}/media/recent'.format(tag)
+        return self._retrieve_photos(url, max_photos)
--- social/tasks/instagram_updater.py
+++ social/tasks/instagram_updater.py
@@ -1,45 +1,80 @@
 import gevent
-from .. import settings
-from ..models import InstagramSearch, InstagramMessage, IGMediaExistsError
-from ..services.instagram import InstagramAPI
+
 from celery.utils.log import get_task_logger
+
+from .. import settings
+from ..models import InstagramAccount, InstagramSearch, InstagramMessage, IGMediaExistsError
+from ..services.instagram import InstagramAPI, RateLimitException
 
 log = get_task_logger(__name__)
 
 
 class InstagramUpdater():
     def __init__(self):
-        self.api = InstagramAPI(app_id=settings.SOCIAL_INSTAGRAM_CLIENT_ID)
+        self._accounts = InstagramAccount.objects.all()
 
     def update(self):
         import gevent.monkey
         gevent.monkey.patch_ssl()
-        threads = []
-        terms = InstagramSearch.objects.all()
 
-        for term in terms:
-            threads.append(gevent.spawn(self._update_term, term))
+        self._update_accounts()
+        self._update_terms()
+
+    def _update_accounts(self):
+        threads = []
+
+        for account in self._accounts:
+            threads.append(gevent.spawn(self._update_account, account))
         gevent.joinall(threads)
 
-    def _update_term(self, term):
-        try:
-            log.info('[instagram] term %s', term.search_term)
-            messages = self.api.tag_recent_media(term.search_term)
-        except Exception as e:
-            log.error('[instagram] error')
-            log.error(e)
-            return
+    def _update_terms(self):
+        threads = []
+        tags = InstagramSearch.objects.all()
 
+        for tag in tags:
+            threads.append(gevent.spawn(self._update_tag, tag))
+        gevent.joinall(threads)
+
+    def _update_account(self, account):
+        log.info('[instagram] updating account "%s"'.format(account.username))
+
+        api = InstagramAPI(account)
+        try:
+            self._iterate_messages(api.scrap_account)
+        except RateLimitException:
+                log.error('[instagram] account "{}" rate limited'.format(account.username))
+        except Exception as exc:
+            log.error('[instagram] error scraping account "{}"'.format(account.username))
+            log.error(exc)
+
+    def _iterate_messages(self, api_method, arguments=[], tag=None):
         message_duplicates = 0
-        for message in messages:
+        for message in api_method(*arguments):
             try:
-                InstagramMessage.create_from_json(message, term)
-                message_duplicates = 0
-            except IGMediaExistsError as e:
+                InstagramMessage.create_from_json(message, tag)
+            except IGMediaExistsError:
                 message_duplicates += 1
                 if message_duplicates >= 5:
                     log.warning('[instagram] you hit 5 duplicates in a row, kicking out')
                     return
-            except Exception as e:
-                log.info('[instagram] larger problem...')
-                log.error(e)
+            else:
+                # Reboot dup count as the last message was successfully
+                # saved.
+                message_duplicates = 0
+
+    def _update_tag(self, tag):
+        log.info('[instagram] searching for tag "%s"'.format(tag.search_term))
+
+        for account in self._accounts:
+            api = InstagramAPI(account)
+            try:
+                self._iterate_messages(api.search_tag, [tag.search_term], tag)
+            except RateLimitException:
+                log.error('[instagram] account "{}" rate limited'.format(account.username))
+            except Exception as exc:
+                log.error('[instagram] error searching for "{}"'.format(tag.search_term))
+                log.error('[instagram] account "{}"'.format(account.username))
+                log.error(exc)
+                return
+            else:
+                return
