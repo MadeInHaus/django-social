76799c78e101fdf5981789ca95e6a48c4fb62471
Joshua Thorp <joshua@stigmergic.net>
2013-10-14 18:01:16 -0600

    fixing instagram video media preview

--- social/utils/instagram.py
+++ social/utils/instagram.py
@@ -8,5 +8,9 @@ def parse_instagram_picture_embed(msg, width="200px", height="200px"):
 
 def parse_instagram_video_embed(msg, width="200px", height="200px"):
     """ need to put in proper video embed here """
-    video = msg['videos']['standard_resolution']['url']
-    return '<video src="{}" width="{}" height="{}">'.format(video, width, height)
+    video_url = msg['videos']['standard_resolution']['url']
+    picture_url = msg['images']['standard_resolution']['url']
+    video_str = """<video controls preload="none" width="{}" height="{}" poster="{}">
+                    <source src="{}" type='video/mp4' />
+                </video>""".format( width, height, picture_url, video_url)
+    return video_str
